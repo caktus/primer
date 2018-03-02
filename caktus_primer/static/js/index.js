1a54387f4fd55b764db4abb9832803e1e98a1e60
philtuskha <phil.tuskha@gmail.com>
2018-03-02 14:25:23 -0500

    set initial value

--- caktus_primer/static/js/index.js
+++ caktus_primer/static/js/index.js
@@ -3,8 +3,11 @@ export function categorySearchSelect(){
 	let selectWrap = document.getElementsByClassName('search-select');
 	for (let el of selectWrap) {
 		let selectElement = el.querySelector('select')
+		// set initially to the selected value
+		el.querySelector('.search-select-display').innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML
+		// add event listener
 		selectElement.addEventListener('change', function() {
-			el.lastElementChild.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML
+			el.querySelector('.search-select-display').innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML
 		});
 	}
 };
